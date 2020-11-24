5070de4021c9007eed46f83dad1c59bcb9a82644
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-11-23 23:30:26 -0900

    Implement while func

--- HW7_alt1/Caesar-cypher.cpp
+++ HW7_alt1/Caesar-cypher.cpp
@@ -14,38 +14,53 @@ void shift(string& caesar, int value) {
 		if (i < 65) {
 			continue;
 		}
-		else if (isupper(i)) {
-			 if (i + value < 91 && i + value > 64){
-				 i = i + value;
-			 }
-			 else {
-				 i = i + value - 26 * (value / 26);
-			 }
+		if (i >= 'a' && i <= 'z') {
+			i = 'a' + ((i - 'a' + value) % 26);
 		}
-		else {
-			if (i + value < 123 && i + value > 96) {
-				i = i + value;
-			}
-			else {
-				i = i + value - 26 * (value / 26);
-			}
+		else if (i >= 'A' && i <= 'Z') {
+			i = 'A' + ((i - 'A' + value) % 26);
 		}
+		else i = i;
 	}
 }
 
 int main() {
-	string input;
-	int num;
-	cout << "Type anything in: ";
-	cin >> input;
-	cout << endl;
-	cout << "Enter a number :";
-	cin >> num;
-	cout << endl;
+	while (true) {
+		string input;
+		int num;
 
-	shift(input, num);
-	cout << input << endl;
-	
-	
+		// string to shift
+		cout << "Type anything in (Enter nothing to exit): ";
+		getline(cin, input);
+		cout << endl;
+
+		// Press enter to quit
+		if (input == "") {
+			cout << "Terminating program..." << endl;
+			break;
+		}
+
+		cin.ignore();
+
+		while (true) {
+			// value of the shift
+			cout << "Enter a number :";
+			cin >> num;
+			cout << endl;
+			if (!isdigit(num)) {
+				break;
+			}
+			else {
+				cout << "Error, please enter a number" << endl;
+			}
+		}
+
+		cin.clear();
+		cin.ignore();
+
+		shift(input, num);
+		cout << input << endl;
+
+	}
 	return 0;
 }
