729bc15bd38b31e7d90e6292c5987282c6359e6f
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-11-23 21:57:13 -0900

    Start

---
+++ HW7_alt1/Caesar-cypher.cpp
@@ -0,0 +1,18 @@
+// Caesar-cypher.cpp Hiromi Kageyama Nov. 23, 2020
+// Write a c++ program that shifts the letters by a certain number
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::endl;
+using std::cin;
+
+int main() {
+	string input;
+	cin >> input;
+
+
+
+	return 0;
+}
