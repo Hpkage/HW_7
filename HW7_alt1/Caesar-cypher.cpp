9bb99b30c3792c83fc5dd28c0147df3373bc9481
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-11-23 22:20:27 -0900

    Add Shift

--- HW7_alt1/Caesar-cypher.cpp
+++ HW7_alt1/Caesar-cypher.cpp
@@ -9,6 +9,23 @@ using std::endl;
 using std::cin;
 using std::cout;
 
+void shift(string& caesar, int value) {
+	for (auto& i: caesar) {
+		if (i + value < 91) {
+			i = i + value;
+		}
+		else if (i + value > 91) {
+			i = i + value - 26;
+		}
+		else if (i + value < 123) {
+			i = i + value;
+		}
+		else if (i + value > 123) {
+			i = i + value - 26;
+		}	
+	}
+}
+
 int main() {
 	string input;
 	int num;
@@ -19,8 +36,9 @@ int main() {
 	cin >> num;
 	cout << endl;
 
-
-
-
+	shift(input, num);
+	cout << input << endl;
+	
+	
 	return 0;
 }
