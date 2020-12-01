68795b2152936bbd76b4d6a28bea3617163b3ef7
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-11-30 23:39:02 -0900

    Create ppm

--- HW7_alt2/Mandelbrot.cpp
+++ HW7_alt2/Mandelbrot.cpp
@@ -22,7 +22,22 @@ int value(int x, int y) {
 }
 
 int main() {
+    ofstream my_Image("mandelbrot.ppm");
 
+    if (my_Image.is_open()) {
+        my_Image << "P3\n" << width << " " << height << " 255\n";
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int val = value(i, j);
+                my_Image << val << ' ' << 0 << ' ' << 0 << "\n";
+            }
+        }
+        my_Image.close();
+    }
+    else
+        cout << "Could not open the file";
+
+    return 0;
 
 
 }
